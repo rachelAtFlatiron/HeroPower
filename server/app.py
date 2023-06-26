1efb49ba507881fbfcd0316a9590bdb3239f6311
Rachel Katz <rachel.katz@flatironschool.com>
2023-06-26 15:45:13 -0400

    demo

--- server/app.py
+++ server/app.py
@@ -17,12 +17,76 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 
+# 🛑 DO NOT FORGET THIS
+api = Api(app)
+
 db.init_app(app)
 
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
+class Heroes(Resource):
+    def get(self):
+        q = Hero.query.all()
+        hero_dict = [h.to_dict(only=('id', 'name', 'super_name')) for h in q]
+        return make_response(hero_dict, 200)
+# 🛑 DONT FORGET add_resource
+api.add_resource(Heroes, '/heroes')
+
+class OneHero(Resource):
+    def get(self, hero_id):
+        q = Hero.query.filter_by(id=hero_id).first()
+        if not q: 
+            return make_response({"error": "Hero not found"}, 404)
+        return make_response(q.to_dict(), 200)
+
+api.add_resource(OneHero, '/heroes/<int:hero_id>')
+
+class Powers(Resource):
+    def get(self):
+        q = Power.query.all()
+        power_dict = [p.to_dict(only=('description', 'id', 'name')) for p in q]
+        return make_response(power_dict, 200)
+api.add_resource(Powers, '/powers')
+
+class OnePower(Resource):
+    def get(self, power_id):
+        q = Power.query.filter_by(id=power_id).first()
+        if not q: 
+            return make_response({"error": "Power not found"}, 404)
+        return make_response(q.to_dict(only=('id', 'description', 'name')), 200)
+    
+    def patch(self, power_id):
+        q = Power.query.filter_by(id=power_id).first()
+        if not q: 
+            return make_response({"error": "Power not found"}, 404)
+        try:
+            data = request.get_json()
+            for attr in data: 
+                setattr(q, attr, data.get(attr)) 
+            db.session.add(q)
+            db.session.commit()
+        except: 
+            return make_response({"errors": ["validation errors"]}, 400)
+
+        return make_response(q.to_dict(only=('description', 'id', 'name')), 200)
+
+api.add_resource(OnePower, '/powers/<int:power_id>')
+
+class HeroPowers(Resource):
+    def post(self):
+        data = request.get_json() 
+        try: 
+            hp = HeroPower(strength=data.get('strength'), power_id=data.get('power_id'), hero_id=data.get('hero_id'))
+            db.session.add(hp)
+            db.session.commit() 
+        except: 
+            return make_response({"errors": ["validation errors"]}, 400)
+
+        return make_response(hp.to_dict(), 200)
+
+api.add_resource(HeroPowers, '/hero_powers')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -19,9 +19,13 @@ class Hero(db.Model, SerializerMixin):
     super_name = db.Column(db.String)
 
     # add relationship
-
+    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade="all, delete-orphan")
+    all_powers = association_proxy('hero_powers', 'power')
     # add serialization rules
+    # Hero.hero_powers <-> HeroPower.hero
+    # Hero.all_powers <-> Power.all_heroes
 
+    serialize_rules = ('-hero_powers.hero', '-all_powers.all_heroes', )
     def __repr__(self):
         return f'<Hero {self.id}>'
 
@@ -34,10 +38,17 @@ class Power(db.Model, SerializerMixin):
     description = db.Column(db.String)
 
     # add relationship
+    hero_powers = db.relationship("HeroPower", back_populates='power', cascade="all, delete-orphan")
+    all_heroes = association_proxy("hero_powers", "hero")
 
     # add serialization rules
-
+    serialize_rules = ( '-hero_powers.power', '-all_heroes.all_powers',)
     # add validation
+    @validates('description')
+    def validates_des(self, key, input_des):
+        if len(input_des) < 20:
+            raise ValueError('no')
+        return input_des
 
     def __repr__(self):
         return f'<Power {self.id}>'
@@ -48,12 +59,26 @@ class HeroPower(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String, nullable=False)
-
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    
     # add relationships
-
+    # these two hero_powers are totally different
+    power = db.relationship("Power", back_populates="hero_powers") #Power.hero_powers
+    hero = db.relationship("Hero", back_populates="hero_powers") #Hero.hero_powers
+    
     # add serialization rules
+    serialize_rules = ('-hero.hero_powers', '-power.hero_powers', )
 
     # add validation
+    @validates('strength')
+    #value is whatever value the user puts in
+    def validate_strength(self, key, input_strength):
+        print(f'from @validates(strength) {input_strength}')
+        # if(input_strength == 'Weak' or input_strength == 'Strong' or input_strength == 'Average'):
+        if(input_strength in ['Weak', 'Average', 'Strong']):
+            return input_strength
+        raise ValueError('no')
 
     def __repr__(self):
         return f'<HeroPower {self.id}>'
